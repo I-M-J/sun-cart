9c729eecbade4f9e9c261c959639af23526a2b16
I-M-J <149041998+I-M-J@users.noreply.github.com>
2026-05-16 10:55:16 +0600

    react-hook-form added for login and register forms

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,6 +1,20 @@
+"use client"
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+
+const onSubmit = (data) => {
+    const { email, password } = data;
+
+    console.log(email, password);
+}
 
 const LoginPage = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm();
+
     return (
         <section className="min-h-[calc(100vh-64px)] flex items-center justify-center bg-bg-muted py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8 bg-white p-8 rounded-2xl shadow-sm border border-stone-100">
@@ -10,20 +24,27 @@ const LoginPage = () => {
                     <p className="mt-2 text-stone-500">Welcome back to SunCart</p>
                 </div>
 
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <fieldset className="fieldset space-y-4">
-
                         <div>
                             <label className="label text-sm font-medium text-stone-700 mb-1">Email Address</label>
-                            <input type="email" className="input input-primary text-base w-full px-4 py-2 border border-stone-300 rounded-lg focus:border-primary" placeholder="Your Email" />
+                            <input
+                                type="email"
+                                className="input text-base w-full px-4 py-2 border border-stone-300 rounded-lg mb-1" placeholder="Your Email"
+                                {...register("email", { required: "Email is required" })} />
+                            {errors.email && <p className="text-danger text-xs">{errors.email.message}</p>}
                         </div>
 
                         <div>
                             <label className="label text-sm font-medium text-stone-700 mb-1">Password</label>
-                            <input type="password" className="input input-primary text-base h-fit w-full px-4 py-2 border border-stone-300 rounded-lg focus:border-primary" placeholder="Password" />
+                            <input
+                                type="password"
+                                className="input text-base h-fit w-full px-4 py-2 border border-stone-300 rounded-lg mb-1" placeholder="Password"
+                                {...register("password", { required: "Password is required" })} />
+                            {errors.password && <p className="text-danger text-xs">{errors.password.message}</p>}
                         </div>
 
-                        <button className="btn h-fit w-full bg-blue-500/90 text-white px-4 py-3 text-base font-semibold rounded-full">Sign In</button>
+                        <button className="btn h-fit w-full bg-blue-500/90 text-white px-4 py-3 text-base font-semibold rounded-full">{isSubmitting ? <span className="loading loading-spinner loading-md text-white"></span> : "Sign In"}</button>
 
                         <button className="btn h-fit w-full bg-primary/80 text-white px-4 py-3 text-base font-semibold rounded-full">Login with Google</button>
                     </fieldset>
--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,39 +1,68 @@
+"use client"
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+
+const onSubmit = (data) => {
+    const { name, photoURL, email, password } = data;
+
+}
 
 const RegisterPage = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting }
+    } = useForm();
+
     return (
         <section className="min-h-[calc(100vh-64px)] flex items-center justify-center bg-bg-muted py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8 bg-white p-8 rounded-2xl shadow-sm border border-stone-100">
                 <div className="text-center">
-                    <h2 className="text-3xl font-bold font-display text-foreground">Sign In</h2>
+                    <h2 className="text-3xl font-bold font-display text-foreground">Create Account</h2>
 
-                    <p className="mt-2 text-stone-500">Welcome back to SunCart</p>
+                    <p className="mt-2 text-stone-500">Welcome to SunCart</p>
                 </div>
 
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <fieldset className="fieldset space-y-4">
 
                         <div>
                             <label className="label text-sm font-medium text-stone-700 mb-1">Name</label>
-                            <input type="email" className="input input-primary text-base w-full px-4 py-2 border border-stone-300 rounded-lg focus:border-primary" placeholder="Your Name" />
+                            <input
+                                type="text"
+                                className="input text-base w-full px-4 py-2 border border-stone-300 rounded-lg mb-1" placeholder="Your Name"
+                                {...register("name", { required: "Name is required" })} />
+                            {errors.name && <p className="text-danger text-xs">{errors.name.message}</p>}
                         </div>
 
                         <div>
                             <label className="label text-sm font-medium text-stone-700 mb-1">Photo URL</label>
-                            <input type="email" className="input input-primary text-base w-full px-4 py-2 border border-stone-300 rounded-lg focus:border-primary" placeholder="Your Photo URL" />
+                            <input
+                                type="text"
+                                className="input text-base w-full px-4 py-2 border border-stone-300 rounded-lg mb-1" placeholder="Your Photo URL"
+                                {...register("photoURL", { required: "Photo URL is required" })} />
+                            {errors.photoURL && <p className="text-danger text-xs">{errors.photoURL.message}</p>}
                         </div>
 
                         <div>
                             <label className="label text-sm font-medium text-stone-700 mb-1">Email Address</label>
-                            <input type="email" className="input input-primary text-base w-full px-4 py-2 border border-stone-300 rounded-lg focus:border-primary" placeholder="Your Email" />
+                            <input
+                                type="email"
+                                className="input text-base w-full px-4 py-2 border border-stone-300 rounded-lg mb-1" placeholder="Your Email"
+                                {...register("email", { required: "Email is required" })} />
+                            {errors.email && <p className="text-danger text-xs">{errors.email.message}</p>}
                         </div>
 
                         <div>
                             <label className="label text-sm font-medium text-stone-700 mb-1">Password</label>
-                            <input type="password" className="input input-primary text-base h-fit w-full px-4 py-2 border border-stone-300 rounded-lg focus:border-primary" placeholder="Password" />
+                            <input
+                                type="password"
+                                className="input text-base h-fit w-full px-4 py-2 border border-stone-300 rounded-lg mb-1" placeholder="Password"
+                                {...register("password", { required: "Password is required" })} />
+                            {errors.password && <p className="text-danger text-xs">{errors.password.message}</p>}
                         </div>
 
-                        <button className="btn h-fit w-full bg-blue-500/90 text-white px-4 py-3 text-base font-semibold rounded-full">Sign In</button>
+                        <button className="btn h-fit w-full bg-blue-500/90 text-white px-4 py-3 text-base font-semibold rounded-full">{isSubmitting ? <span className="loading loading-spinner loading-md text-white"></span> : "Sign Up"}</button>
 
                         <button className="btn h-fit w-full bg-primary/80 text-white px-4 py-3 text-base font-semibold rounded-full">Login with Google</button>
                     </fieldset>
